c37b875999882d74820d63fa133f4bbfbdcdb849
lizhiyao <lizy1993@163.com>
2016-03-11 09:25:35 +0800

    init helloworld
    Signed-off-by: lizhiyao <lizy1993@163.com>

---
+++ build/.module-cache/bb1347762c36de4efb615b55f6efc3672c38272e.js
@@ -0,0 +1,4 @@
+React.render(
+    React.createElement("h1", null, "Hello, lizhiyao! How are u?"),
+    document.getElementById('example')
+);
---
+++ src/helloworld.js
@@ -0,0 +1,4 @@
+React.render(
+    <h1>Hello, lizhiyao! How are u?</h1>,
+    document.getElementById('example')
+);
