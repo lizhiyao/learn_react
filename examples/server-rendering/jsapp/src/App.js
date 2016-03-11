30d35512db7689651ff3a92e27995dfa97b3a716
lizhiyao <lizy1993@163.com>
2016-03-11 10:29:57 +0800

    comments example start
    Signed-off-by: lizhiyao <lizy1993@163.com>

--- examples/server-rendering/jsapp/src/App.js
+++ examples/server-rendering/jsapp/src/App.js
@@ -3,9 +3,9 @@
 var React = require('react');
 
 var App = React.createClass({
-  render: function() {
-    return <h1>Hello {this.props.name}!</h1>;
-  }
+    render: function () {
+        return <h1>Hello {this.props.name}!</h1>;
+    }
 });
 
 module.exports = App;
