b165a936741c5643f86ef595d11c5921e879bced
lizhiyao <lizy1993@163.com>
2016-03-23 23:37:27 +0800

    comment local ok
    Signed-off-by: lizhiyao <lizy1993@163.com>

--- comment/public/js/comment.js
+++ comment/public/js/comment.js
@@ -3,16 +3,69 @@
  */
 
 // 原生 HTML 元素名以小写字母开头，而自定义的 React 类名以大写字母开头
-var CommentBox = React.createClass({
-    render: function () {
+var data = [
+    { author: "Pete Hunt", text: "This is one comment" },
+    { author: "Jordan Walke", text: "This is *another* comment" }
+];
+
+var Comment = React.createClass({
+    rawMarkup: function() {
+        var rawMarkup = marked(this.props.children.toString(), { sanitize: true });
+        return { __html: rawMarkup };
+    },
+
+    render: function() {
         return (
-            <div className="commentBox">
-                Hello, world! I'm a CommentBox.
+            <div className="comment">
+                <h2 className="commentAuthor">
+                    {this.props.author}
+                </h2>
+                <span dangerouslySetInnerHTML={this.rawMarkup() } />
             </div>
         );
     }
 });
-React.render(
-    <CommentBox />,
+
+var CommentList = React.createClass({
+    render: function() {
+        var commentNodes = this.props.data.map(function(comment) {
+            return (
+                <Comment author={comment.author}>
+                    {comment.text}
+                </Comment>
+            )
+        });
+        return (
+            <div className="commentList">
+                {commentNodes}
+            </div>
+        );
+    }
+});
+
+var CommentForm = React.createClass({
+    render: function() {
+        return (
+            <div className="commentForm">
+                I'm a CommentForm.
+            </div>
+        );
+    }
+});
+
+var CommentBox = React.createClass({
+    render: function() {
+        return (
+            <div className="commentBox">
+                <h1>Comments</h1>
+                <CommentList data={this.props.data} />
+                <CommentForm />
+            </div>
+        );
+    }
+});
+ReactDOM.render(
+    <CommentBox data={data} />,
     document.getElementById('content')
 );
+
