54a121b68dd583e89eb7c11e50afffb568394671
lizhiyao <lizy1993@163.com>
2016-03-23 22:52:17 +0800

    Hello, world! I'm a CommentBox.
    Signed-off-by: lizhiyao <lizy1993@163.com>

--- comment/public/js/comment.js
+++ comment/public/js/comment.js
@@ -2,3 +2,17 @@
  * Created by lizhiyao on 2016/3/23.
  */
 
+// 原生 HTML 元素名以小写字母开头，而自定义的 React 类名以大写字母开头
+var CommentBox = React.createClass({
+    render: function () {
+        return (
+            <div className="commentBox">
+                Hello, world! I'm a CommentBox.
+            </div>
+        );
+    }
+});
+React.render(
+    <CommentBox />,
+    document.getElementById('content')
+);
